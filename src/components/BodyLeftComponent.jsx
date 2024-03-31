35f8b6bb93b86d33602cf809247b2ce078e7a3f6
ShikhaSingh1807 <shikha1807@gmail.com>
2024-04-01 00:26:14 +0530

    first commit

---
+++ src/components/BodyLeftComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BodyLeftComponent() {
+    return (
+        <div>
+            <h3>I am a Body Left Component !!!</h3>
+        </div>
+    );
+}
---
+++ src/components/BodyRightComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BodyRightComponent() {
+    return (
+        <div>
+            <h3>I am a Body Right Component !!!</h3>
+        </div>
+    )
+}
